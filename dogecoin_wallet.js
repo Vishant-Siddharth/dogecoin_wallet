985c5b9b7d4c6cbcf4c6b932699191408ad4cf0b
vishant <vishant@buyucoin.cojm>
2018-05-18 00:21:20 +0530

    addding dogecoin_wallet

--- dogecoin_wallet.js
+++ dogecoin_wallet.js
@@ -32,7 +32,7 @@ var genrate_address = function(api_key, pass, lable)
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 var get_address_by_label = function(api_key, pass, lable)
-    {
+{
     var get_add_by_label = function(api_key, pass, lable){
         return new Promise(function(resolve, reject)
         {
@@ -63,7 +63,7 @@ var get_address_by_label = function(api_key, pass, lable)
 };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
-var get_available_balance = function( address, lable, id)
+var get_available_balance = function(api_key, pass, address, lable, id)
 {
     var get_available_bal = function(api_key, pass, address='False', lable='False', id='False'){
         return new Promise(function(resolve, reject)
@@ -77,8 +77,8 @@ var get_available_balance = function( address, lable, id)
                     resolve(res.data.error_message);
                 reject(err);
             };
-            var api_key = '2b5b-0dff-6e47-c216';
-            var pass = 'vector31';
+//            var api_key = '2b5b-0dff-6e47-c216';
+//            var pass = 'vector31';
             var ans;
             var BlockIo = require('block_io');
             var version = 2;
@@ -107,7 +107,7 @@ var get_available_balance = function( address, lable, id)
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
-var pending_received_balance = function( address, lable, id)
+var pending_received_balance = function(api_key, pass, address, lable, id)
 {
     var pending_received_bal = function(api_key, pass, address = 'False', lable = 'False', id = 'False'){
         return new Promise(function(resolve, reject)
@@ -115,13 +115,16 @@ var pending_received_balance = function( address, lable, id)
             function response(err, res)
             {
                 console.log("res: ", res);
-                resolve(res.data.pending_received_balance);
+                if (res.data.pending_received_balance)
+                    resolve(res.data.pending_received_balance);
+                 else
+                    resolve(res.data.error_message);
                 reject(err);
             };
 
             console.log(lable, " ", address, " ", id);
-            var api_key = '2b5b-0dff-6e47-c216';
-            var pass = 'vector31';
+//            var api_key = '2b5b-0dff-6e47-c216';
+//            var pass = 'vector31';
             var BlockIo = require('block_io');
             var version = 2;
             var ans;
@@ -149,42 +152,39 @@ var pending_received_balance = function( address, lable, id)
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////
-var get_withdraw = function(amounts, to_addresses, pin)
+var get_withdrawl = function(api_key, pass, amounts, to_addresses, pin)
 {
-    var withdraw_from_add = function(api_key, pass, amounts, to_addresses, pin){
-        return new Promise(function(resolve, reject)
-        {
-            function response(err, res)
-            {
-                console.log("res: ", res);
-                if (res.data.address)
-                    resolve(res.data.address);
-                else
-                    resolve(res.data.error_message);
-                reject(err);
-            };
-            var api_key = '2b5b-0dff-6e47-c216';
-            var pass = 'vector31';
-            var BlockIo = require('block_io');
-            var version = 2;
-            var ans;
-            var block_io = new BlockIo(api_key, pass, version);
-            var f_add = from_addresses.join();
-            var t_add = to_addresses.join();
-            var amo = amounts.join();
-            block_io.withdraw({'amounts': amo, 'to_addresses': t_add, 'pin': pin}, response);
-        })
-    };
-
-    withdraw_from_address(api_key, pass, amounts, to_addresses, pin)
-        .then(
-            function(result){ans = result; console.log(`end: ${ans}`); },
-            function(err){ console.log(err); console.log('error'); }
-        )
+//    var api_key = '2b5b-0dff-6e47-c216';
+//    var pass = 'vector31';
+    var amount = amounts.join();
+    var to_address = to_addresses.join();
+    var BlockIo = require('block_io');
+    var version = 2;
+    var ans;
+    var block_io = new BlockIo(api_key, pass, version);
+    url = "https://block.io/api/v2/withdraw/?api_key=" + api_key + "&amounts=" + amount + "&to_addresses=" + to_address + "&pin=" + pin
+    var request = require('request');
+    request(url, function (error, response, body) {
+//      console.log('error:', error); // Print the error if one occurred
+//      console.log('statusCode:', response); // Print the response status code if a response was received
+//      console.log('body:', body ); // Print the HTML for the Google homepage.
+      var jsonObject = JSON.parse(body);
+      console.log('status: ', jsonObject);
+      if (jsonObject['data']['txid'])
+      {
+        console.log('txid', jsonObject['data']['txid']);
+        ans = jsonObject['data']['txid'];
+      }
+      else
+      {
+        console.log('error_message', jsonObject['data']['error_message']);
+        ans = jsonObject['data']['error_message'];
+      }
+    });
 };
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
-var get_transactions = function(TXID)
+var get_transactions_detail = function(api_key, pass, TXID)
 {
     var get_transaction = function(api_key, pass, TXID){
         return new Promise(function(resolve, reject)
@@ -198,16 +198,15 @@ var get_transactions = function(TXID)
                     resolve(res.data.error_message);
                 reject(err);
             };
+            var api_key = '2b5b-0dff-6e47-c216';
+            var pass = 'vector31';
+            var BlockIo = require('block_io');
+            var version = 2;
+            var ans;
+            var block_io = new BlockIo(api_key, pass, version);
             block_io.get_transactions({'type': 'sent', 'before_tx': TXID}, response);
         })
     };
-
-    var api_key = '2b5b-0dff-6e47-c216';
-    var pass = 'vector31';
-    var BlockIo = require('block_io');
-    var version = 2;
-    var ans;
-    var block_io = new BlockIo(api_key, pass, version);
     get_transaction(api_key, pass, TXID)
         .then(
             function(result){ans = result; console.log(`end: ${ans}`); },
@@ -217,8 +216,8 @@ var get_transactions = function(TXID)
 
 //module.exports =
 module.exports.get_available_balance = get_available_balance;
-module.exports.get_transactions = get_transactions;
-module.exports.get_withdraw = get_withdraw;
+module.exports.get_transactions_detail = get_transactions_detail;
+module.exports.get_withdrawl = get_withdrawl;
 module.exports.get_address_by_label = get_address_by_label;
 module.exports.pending_received_balance = pending_received_balance;
 module.exports.get_address_by_label = get_address_by_label;
