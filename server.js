0475c5c846bf70668359a0b4cb62ab499378ea5c
Devesh Aggrawal <deveshaggrawal19@gmail.com>
2018-05-21 15:32:06 +0530

    adding https server for adding socket io

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var DogeCoin = new require('./dogecoin_wallet.js');
-
+var port = process.env.PORT || 5000;
+var host = process.env.HOST || "0.0.0.0";
 
 const app = express();
 const dogecoin = new DogeCoin({api_key:'b270-9972-3478-2fa8', pass:'vishant3731', version:2});
@@ -32,14 +33,12 @@ app.get('/get-label/:label', (req, res)=>{
     dogecoin.get_address_by_label(req.params.label, send_json);
 });
 
+var server = require('http').createServer(app);
 
-app.listen(5000, "0.0.0.0", (err, data) =>
-    {
-        if(err){
+
+server.listen(port, host, (err, data) => {
+    if(err){
         console.error(err);
-        }
-        else{
-        console.log('Server is up...');
-        }
     }
-);
+    console.log('Server listening at http://%s:%d', host, port);
+});
