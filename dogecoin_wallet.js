d676f0954c6d9c5a58c2ec8531bc248cf3f13ccc
vishant <vishant@buyucoin.cojm>
2018-05-23 15:47:16 +0530

    adding working routes for express, 5

--- dogecoin_wallet.js
+++ dogecoin_wallet.js
@@ -1,31 +1,43 @@
 "use strict";
 var throw_error = require('./custom_error');
 
-class DogeCoin {
-
-    constructor (obj){
-        if (!obj){
+class DogeCoin
+{
+    constructor (obj)
+    {
+        if (!obj)
+        {
             throw new throw_error('Please pass the dictionary', 500)
         }
-        if ((!obj.api_key || !obj.pass || !obj.version)){
+        if ((!obj.api_key || !obj.pass || !obj.version))
+        {
             throw new throw_error('Not all parameters were passed', 500)
         }
+        this.PIN = obj.pass;
         var BlockIo = require('block_io');
         this.block_io = new BlockIo(obj.api_key, obj.pass, obj.version);
     }
 
-    generate_address(label, call_back_function){
-        var generate_add = (label) => {
+    generate_address(label, call_back_function)
+    {
+        var generate_add = (label) =>
+        {
             var self = this;
-            return new Promise(function(resolve, reject){
-                function response(err, res){
-                    if (err){
+            return new Promise(function(resolve, reject)
+            {
+                function response(err, res)
+                {
+                    if (err)
+                    {
                         reject(err);
                     }
-                    if (res.data.address){
+                    if (res.data.address)
+                    {
+                        let val = res.data.address + "-" + res.data.user_id
                         resolve(res.data.address);
                     }
-                    else{
+                    else
+                    {
                         var err = new throw_error(res.data.error_message, 500)
                         reject(err);
                     }
@@ -39,18 +51,25 @@ class DogeCoin {
 
     };
 
-    get_address_by_label(label, call_back_function){
-        var get_add_by_label = (label) => {
+    get_address_by_label(label, call_back_function)
+    {
+        var get_add_by_label = (label) =>
+        {
             var self = this;
-            return new Promise(function(resolve, reject){
-                function response(err, res){
-                    if (err){
+            return new Promise(function(resolve, reject)
+            {
+                function response(err, res)
+                {
+                    if (err)
+                    {
                         reject(err);
                     }
-                    if (res.data.address){
+                    if (res.data.address)
+                    {
                         resolve(res.data.address);
                     }
-                    else{
+                    else
+                    {
                         reject(new throw_error(res.data.error_message, 500));
                     }
                 };
@@ -62,149 +81,134 @@ class DogeCoin {
             .catch((err) => {if(err){console.log('asdf');}call_back_function(err, null);});
     };
 
+    get_available_balance(address, call_back_function)
+    {
+        var get_available_bal = (address) =>
+        {
+            var self = this;
+            return new Promise(function(resolve, reject)
+            {
+                function response(err, res)
+                {
+                    if(err)
+                    {
+                        reject(err);
+                    }
+                    if (res.data.available_balance)
+                    {
+                        resolve(res.data.available_balance);
+                    }
+                    else
+                    {
+                        reject(new throw_error(res.data.error_message, 500));
+                    }
+                };
+                self.block_io.get_address_balance({'address':address}, response);
+            })
+        };
+
+        get_available_bal(address)
+            .then((result) =>{call_back_function(null, result);})
+            .catch((err) => {if(err){console.log('asdf');}call_back_function(err, null);});
+    };
+
+    pending_received_balance(address, call_back_function)
+    {
+        var pending_received_bal = (address) =>
+        {
+            var self = this;
+            return new Promise(function(resolve, reject)
+            {
+                function response(err, res)
+                {
+                    if(err)
+                    {
+                        reject(err);
+                    }
+                    if (res.data.pending_received_balance)
+                    {
+                        resolve(res.data.pending_received_balance);
+                    }
+                    else
+                    {
+                        reject(new throw_error(res.data.error_message, 500));
+                    }
+                };
+                self.block_io.get_address_balance({'address':address}, response);
+            })
+        };
+
+        pending_received_bal(address)
+            .then((result) =>{call_back_function(null, result);})
+            .catch((err) => {if(err){console.log('asdf');}call_back_function(err, null);});
+    };
+
+    withdrawl_balance(dic, call_back_function)
+    {
+        let address = dic.to_addresses
+        let amount = dic.amount
+//        let pin = this.PIN
+
+        var withdrawl_bal = () =>
+        {
+            var self = this;
+            return new Promise(function(resolve, reject)
+            {
+                function response(err, res)
+                {
+                    if(err)
+                    {
+                        reject(err);
+                    }
+                    if (res.data.txid)
+                    {
+                        resolve(res.data.txid);
+                    }
+                    else
+                    {
+                        reject(new throw_error(res.data.error_message, 500));
+                    }
+                };
+                self.block_io.withdraw({'amount':amount, 'to_addresses':address, 'PIN':self.pin}, response);
+            })
+        };
+        withdrawl_bal()
+            .then((result) =>{call_back_function(null, result);})
+            .catch((err) => {if(err){console.log('asdf');}call_back_function(err, null);});
+    };
+
+    get_transaction_details(txid, call_back_function)
+    {
+        var transaction_details = (txid) =>
+        {
+            return new Promise(function(resolve, reject)
+            {
+                let url = "https://dogechain.info/api/v1/transaction/" + txid
+//                console.log("url: ", url);
+                var request = require('request');
+                request(url, function (error, response, body)
+                {
+                    if (error != null)
+                    {
+                        console.log('error: ', error);
+                        reject(error)
+                    }
+                    let jsonObject = JSON.parse(body);
+                    if (jsonObject.transaction && jsonObject.transaction.confirmations)
+                    {
+                        resolve(jsonObject['transaction']['confirmations']);
+                    }
+                    else
+                        reject(jsonObject['error']);
+                });
+            });
+        };
+        transaction_details(txid)
+            .then((result) =>{call_back_function(null, result);})
+            .catch((err) => {if(err){console.log('asdf');}call_back_function(err, null);});
+    };
 }
-///////////////////////////////////////////////////////////////////////////////////////////////
-var get_available_balance = function(api_key, pass, address, lable, id)
-{
-    var get_available_bal = function(api_key, pass, address='False', lable='False', id='False'){
-        return new Promise(function(resolve, reject)
-        {
-            function response(err, res)
-            {
-                console.log("res: ", res);
-                if (res.data.available_balance)
-                    resolve(res.data.available_balance);
-                else
-                    resolve(res.data.error_message);
-                reject(err);
-            };
-            var ans;
-            var BlockIo = require('block_io');
-            var version = 2;
-            var block_io = new BlockIo(api_key, pass, version);
-//            console.log(api_key, pass);
-            if (address != 'False')
-                { console.log('address');
-                block_io.get_address_balance({'address':address}, response);}
-            else if (lable != 'False')
-                { console.log('lable');
-                block_io.get_address_balance({'lable':lable}, response);}
-            else if (id != 'False')
-                { console.log('id');
-                block_io.get_address_balance({'user_id':id}, response);}
-            else
-                { console.log('gfd');
-                block_io.get_balance({}, response);}
-        })
-    };
-
-    get_available_bal( address = 'False',lable = 'default', id = 'False')
-        .then(
-            function(result){ans = result; console.log(`end: ${ans}`); },
-            function(err){ console.log(err); console.log('error'); }
-        )
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////
-var pending_received_balance = function(api_key, pass, address, lable, id)
-{
-    var pending_received_bal = function(api_key, pass, address = 'False', lable = 'False', id = 'False'){
-        return new Promise(function(resolve, reject)
-        {
-            function response(err, res)
-            {
-                console.log("res: ", res);
-                if (res.data.pending_received_balance)
-                    resolve(res.data.pending_received_balance);
-                 else
-                    resolve(res.data.error_message);
-                reject(err);
-            };
-
-//            console.log(lable, " ", address, " ", id);
-            var BlockIo = require('block_io');
-            var version = 2;
-            var ans;
-            var block_io = new BlockIo(api_key, pass, version);
-            if (lable != 'False')
-                { console.log('lable');
-                block_io.get_address_balance({'lable':lable}, response);}
-            else if (address != 'False')
-                { console.log('address');
-                block_io.get_address_balance({'address':address}, response);}
-            else if (id != 'False')
-                { console.log('id');
-                block_io.get_address_balance({'user_id':id}, response);}
-            else
-                { console.log('gfd');
-                block_io.get_balance({}, response);}
-        })
-    };
-
-    pending_received_bal(api_key, pass, address = 'False', lable='False', id = 'False')
-        .then(
-            function(result){ans = result; console.log(`end: ${ans}`); },
-            function(err){ console.log(err); console.log('error'); }
-        )
-};
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-var get_withdrawl = function(api_key, pass, amounts, to_addresses, pin)
-{
-    var amount = amounts.join();
-    var to_address = to_addresses.join();
-    var BlockIo = require('block_io');
-    var version = 2;
-    var ans;
-    var block_io = new BlockIo(api_key, pass, version);
-    url = "https://block.io/api/v2/withdraw/?api_key=" + api_key + "&amounts=" + amount + "&to_addresses=" + to_address + "&pin=" + pin
-    var request = require('request');
-    request(url, function (error, response, body) {
-//      console.log('error:', error); // Print the error if one occurred
-//      console.log('statusCode:', response); // Print the response status code if a response was received
-//      console.log('body:', body ); // Print the HTML for the Google homepage.
-      var jsonObject = JSON.parse(body);
-      console.log('status: ', jsonObject);
-      if (jsonObject['data']['txid'])
-      {
-        console.log('txid', jsonObject['data']['txid']);
-        ans = jsonObject['data']['txid'];
-      }
-      else
-      {
-        console.log('error_message', jsonObject['data']['error_message']);
-        ans = jsonObject['data']['error_message'];
-      }
-    });
-};
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-var get_transactions_detail = function(api_key, pass, TXID)
-{
-    var get_transaction = function(api_key, pass, TXID){
-        return new Promise(function(resolve, reject)
-        {
-            function response(err, res)
-            {
-                console.log("res: ", res);
-                if (res.status)
-                    resolve(res.status);
-                else
-                    resolve(res.data.error_message);
-                reject(err);
-            };
-            var BlockIo = require('block_io');
-            var version = 2;
-            var ans;
-            var block_io = new BlockIo(api_key, pass, version);
-            block_io.get_transactions({'type': 'sent', 'before_tx': TXID}, response);
-        })
-    };
-    get_transaction(api_key, pass, TXID)
-        .then(
-            function(result){ans = result; console.log(`end: ${ans}`); },
-            function(err){ console.log(err); console.log('error'); }
-        )
-};
 
 module.exports = DogeCoin
+
+
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var DogeCoin = new require('./dogecoin_wallet.js');
 var port = process.env.PORT || 5000;
 var host = process.env.HOST || "0.0.0.0";
-var api_key = process.env.DOGECOIN_MAIN_KEY || 'b270-9972-3478-2fa8';
-var pass = process.env.DOGECOIN_MAIN_PASS || 'vishant3731';
+var api_key = process.env.DOGECOIN_MAIN_KEY || '*******************';
+var pass = process.env.DOGECOIN_MAIN_PASS || '#####################';
 
 const app = express();
 const dogecoin = new DogeCoin({api_key:api_key, pass:pass, version:2});
@@ -15,19 +15,26 @@ app.get('/', function(req, res){
 
 
 app.get('/create-address/:label', (req, res)=>{
-    function send_json(err, data){
-        if(err){
+    function send_json(err, data)
+    {
+        if(err)
+        {
             res.json({status: "error", message: err.message});
         }
-        res.json({status: "success", address: data, label: req.params.label});
+        var arr = data.split("-");
+        let add = arr[0];
+        let user_id = arr[1];
+        res.json({status: "success",id: user_id, address: add, label: req.params.label});
     }
     dogecoin.generate_address(req.params.label, send_json);
 });
 
 
-app.get('/get-label/:label', (req, res)=>{
-    function send_json(err, data){
-        if(err){
+app.get('/get-address-using-lable/:label', (req, res)=>{
+    function send_json(err, data)
+    {
+        if(err)
+        {
             res.json({status: "error", message: err.message});
         }
         res.json({status: "success", address: data, label: req.params.label});
@@ -35,8 +42,57 @@ app.get('/get-label/:label', (req, res)=>{
     dogecoin.get_address_by_label(req.params.label, send_json);
 });
 
-var server = require('http').createServer(app);
+app.get('/get-available-balance/:add', (req, res)=>{
+    function send_json(err, data)
+    {
+        if(err)
+        {
+            res.json({status:"error", message: err.message});
+        }
+        res.json({status: "success", available_balance: data});
+    }
+    dogecoin.get_available_balance(req.params.add, send_json);
+});
 
+app.get('/get-pending-balance/:add', (req, res)=>{
+    function send_json(err, data)
+    {
+        if(err)
+        {
+            res.json({status:"error", message: err.message});
+        }
+        res.json({status: "success", pending_received_balance: data});
+    }
+    dogecoin.pending_received_balance(req.params.add, send_json);
+});
+
+app.get('/withdrawl-balance/:amount/:to_addresses', (req, res)=>{
+    function send_json(err, data)
+    {
+        if(err)
+        {
+            res.json({status:"error", message: err.message});
+        }
+        res.json({status: "success", txid: data});
+    }
+    console.log(req.params)
+    dogecoin.withdrawl_balance(req.params, send_json);
+});
+
+app.get('/transaction/:txid', (req, res)=>{
+    function send_json(err, data)
+    {
+        if(err)
+        {
+            res.json({status:"error", message: err.message});
+        }
+        res.json({status: "success", confirmations: data});
+    }
+    dogecoin.get_transaction_details(req.params.txid, send_json);
+});
+
+
+var server = require('http').createServer(app);
 
 server.listen(port, host, (err, data) => {
     if(err){
